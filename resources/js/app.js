c20fbcaaca5fe3ddcbce5ad194de2ad089e95cbe
AdrianeZ <eddy122394@gmail.com>
2022-03-22 15:59:31 +0100

    integrated sass with project, made basic styling and navbar component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 require('./bootstrap');
+
+const mainMenu = document.querySelector(".main-header__menu");
+const hamburgerMenu = document.querySelector(".main-header__burger");
+const closeMenuButton = document.querySelector(".main-header__close");
+
+
+hamburgerMenu.addEventListener("click", () =>
+{
+    mainMenu.classList.add("mobile");
+    closeMenuButton.style.display = "block";
+});
+
+closeMenuButton.addEventListener("click", (e) =>
+{
+    mainMenu.classList.remove("mobile");
+    closeMenuButton.style.display = "none";
+});
+
+
+
+
